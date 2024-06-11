6acfb356e81711dac2ffb041101ce00e06c1f557
datanh98hp <dat198hp@gmail.com>
2024-06-11 13:10:30 +0700

    fix data return []

--- src/app/members/page.tsx
+++ src/app/members/page.tsx
@@ -1,18 +1,24 @@
 import { columns } from "@/components/custome/DatatableContainer/columns";
 import { DataTable } from "@/components/custome/DatatableContainer/data-table";
+import { headers } from "next/headers";
 import React from "react";
 
 export const metadata = {
   title: "Members",
 };
 export async function getDataMembers() {
-  const res = await fetch("http://localhost:3000/api/member");
+  const baseURL = headers().get("referer")?.split("/").slice(0, 3).join("/");
+  const res = await fetch(`${baseURL}/api/member`);
+  // const res = await fetch(`${process.env.API_BASE_URI}/api/member`);
+  if (!res.ok) {
+    throw new Error("Failed to fetch data");
+  }
   const data = await res.json();
   return data;
 }
 export default async function PerformPage() {
   const data = await getDataMembers();
-  console.log(data);
+
   return (
     <div>
       <DataTable columns={columns} data={data} />
